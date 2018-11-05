74c108d750d1a0f3026134cc9aed846f4890bf33
vryanblanc <blanc@lacroixrouge-brest.fr>
2018-11-05 16:18:16 +0100

    creation de l'IHM de base : 4 boutons et 2 input text pour 2 moteurs

---
+++ index.php
@@ -0,0 +1,15 @@
+<html> 
+	<head> 
+		<title> Mon premier Site Web </title> 
+	</head> 
+		<body> 
+			<div id="titre1" classe="titre" style = "border:thin solid">
+				<div id="case1" classe="case" style = "border:thin solid"> <h1> Moteur 1 </h1> </div>
+				<div id="case2" classe="case" style = "border:thin solid"> <input type="button=+"/> <input type="text=vitesse"/> <input type="button=-"/> <input type="button=stop"/></div>
+			</div> 
+			<?php
+				echo "Nous sommes aujourd'hui le " . date("l").".";
+			?>
+		</body> 
+
+</html>
